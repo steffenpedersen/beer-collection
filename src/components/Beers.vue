5bc4a8e66c6705fd5b0cb49c0d498183c1756b04
steffenpedersen <steffen.pedersen@live.dk>
2019-11-18 12:48:19 +0100

    Add dummy data to beer collection

--- src/components/Beers.vue
+++ src/components/Beers.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
       <h3>Beers</h3>
+        <div v-for="beer in allBeers" :key="beer.id">
+            {{ beer.title }}
+        </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
-    name: 'Beers'
+    name: 'Beers',
+    computed: mapGetters(['allBeers'])
 }
 </script>
 
--- src/store/modules/beers.js
+++ src/store/modules/beers.js
@@ -1,6 +1,19 @@
-const state = {};
+const state = {
+    beers: [
+        {
+            id: 1,
+            title: 'Beer One'
+        },
+        {
+            id: 2,
+            title: 'Beer Two'
+        }
+    ]
+};
 
-const getters = {};
+const getters = {
+    allBeers: state => state.beers
+};
 
 const actions = {};
 
