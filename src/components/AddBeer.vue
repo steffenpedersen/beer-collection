9723991a34e5087933257050f26bb56c84fc73c3
steffenpedersen <steffen.pedersen@live.dk>
2019-11-18 17:45:12 +0100

    Add beer functionality

--- src/components/AddBeer.vue
+++ src/components/AddBeer.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
-      <h3>Add Beer</h3>
+    <h3>Add Beer</h3>
+
+    <form @submit="onSubmit">
+        <input type="text" v-model="name" placeholder="Add beer">
+        <input type="submit" value="Submit">
+    </form>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
-    name: 'Add Beer'
-}
+  name: 'AddBeer',
+  data() {
+    return {
+      name: ''
+    };
+  },
+  methods: {
+    ...mapActions(['addBeer']),
+    onSubmit(e) {
+      e.preventDefault();
+      this.addBeer(this.name);
+    }
+  }
+};
 </script>
 
 <style>
--- src/store/modules/beers.js
+++ src/store/modules/beers.js
@@ -9,19 +9,31 @@ const getters = {
 };
 
 const actions = {
-    async getBeers({ commit }) {
+      async getBeers({ commit }) {
         const response = await axios.get('http://localhost:8080/v2/beers/', {
             params: {
                 key: `${process.env.VUE_APP_API_KEY}`
-              }          
+              }
         });
 
         commit('setBeers', response.data.data)
-    }
+      },
+      async addBeer({ commit }, name) {
+        // Note: BreweryDB doesn't support post
+        // READ ONLY MODE: The beer has been successfully added
+        // and is waiting to be approved by our administrators
+        const response = {
+            id: 1,
+            name: name
+        }
+
+        commit('newBeer', response);
+      }
 };
 
 const mutations = {
-    setBeers: (state, beers) => (state.beers = beers)
+    setBeers: (state, beers) => (state.beers = beers),
+    newBeer: (state, beer) => state.beers.unshift(beer)
 };
 
 export default {
