9e266bf5fc7d6413582fffe6b6a770d34312dcee
steffenpedersen <steffen.pedersen@live.dk>
2019-11-18 14:57:04 +0100

    Add initial AddBeer component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <AddBeer/>
     <Beers/>
   </div>
 </template>
 
 <script>
 import Beers from './components/Beers.vue'
+import AddBeer from './components/AddBeer.vue'
 
 export default {
   name: 'app',
   components: {
-    Beers
+    Beers,
+    AddBeer
   }
 }
 </script>
---
+++ src/components/AddBeer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h3>Add Beer</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Add Beer'
+}
+</script>
+
+<style>
+
+</style>
